e1b71541aad9a7be8ed76358fcc5ae3ea48a246a
monosoul <Kloz.Klaud@gmail.com>
2022-06-01 17:40:56 +0200

    Introduce JavaBasedConfigJooqDockerPluginFunctionalTest

---
+++ src/test/kotlin/dev/monosoul/jooq/functional/JavaBasedConfigJooqDockerPluginFunctionalTest.kt
@@ -0,0 +1,105 @@
+package dev.monosoul.jooq.functional
+
+import org.gradle.testkit.runner.TaskOutcome.SUCCESS
+import org.junit.jupiter.api.Test
+import strikt.api.expect
+import strikt.assertions.isEqualTo
+import strikt.java.exists
+import strikt.java.notExists
+
+class JavaBasedConfigJooqDockerPluginFunctionalTest : JooqDockerPluginFunctionalTestBase() {
+
+    @Test
+    fun `should respect the generator customizations`() {
+        // given
+        prepareBuildGradleFile {
+            """
+                plugins {
+                    id("dev.monosoul.jooq-docker")
+                }
+
+                repositories {
+                    mavenCentral()
+                }
+
+                tasks {
+                    generateJooqClasses {
+                        schemas = arrayOf("public", "other")
+                        generateUsingJavaConfig {
+                            database.withExcludes("BAR")
+                        }
+                    }
+                }
+
+                dependencies {
+                    jdbc("org.postgresql:postgresql:42.3.6")
+                }
+            """.trimIndent()
+        }
+        copyResource(
+            from = "/V01__init_multiple_schemas.sql",
+            to = "src/main/resources/db/migration/V01__init_multiple_schemas.sql"
+        )
+
+        // when
+        val result = runGradleWithArguments("generateJooqClasses")
+
+        // then
+        expect {
+            that(result).generateJooqClassesTask.outcome isEqualTo SUCCESS
+            that(
+                projectFile("build/generated-jooq/org/jooq/generated/public_/tables/Foo.java")
+            ).exists()
+            that(
+                projectFile("build/generated-jooq/org/jooq/generated/other/tables/Bar.java")
+            ).notExists()
+        }
+    }
+
+    @Test
+    fun `should respect the generator customizations when using deprecated method`() {
+        // given
+        prepareBuildGradleFile {
+            """
+                plugins {
+                    id("dev.monosoul.jooq-docker")
+                }
+
+                repositories {
+                    mavenCentral()
+                }
+
+                tasks {
+                    generateJooqClasses {
+                        schemas = arrayOf("public", "other")
+                        customizeGenerator {
+                            database.withExcludes("BAR")
+                        }
+                    }
+                }
+
+                dependencies {
+                    jdbc("org.postgresql:postgresql:42.3.6")
+                }
+            """.trimIndent()
+        }
+        copyResource(
+            from = "/V01__init_multiple_schemas.sql",
+            to = "src/main/resources/db/migration/V01__init_multiple_schemas.sql"
+        )
+
+        // when
+        val result = runGradleWithArguments("generateJooqClasses")
+
+        // then
+        expect {
+            that(result).generateJooqClassesTask.outcome isEqualTo SUCCESS
+            that(
+                projectFile("build/generated-jooq/org/jooq/generated/public_/tables/Foo.java")
+            ).exists()
+            that(
+                projectFile("build/generated-jooq/org/jooq/generated/other/tables/Bar.java")
+            ).notExists()
+        }
+    }
+}

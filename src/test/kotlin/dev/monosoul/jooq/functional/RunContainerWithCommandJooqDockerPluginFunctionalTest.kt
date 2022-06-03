236a17f4ec2130d8afedaa8e925927f66ae99f1e
monosoul <Kloz.Klaud@gmail.com>
2022-06-03 17:51:12 +0200

    Introduce RunContainerWithCommandJooqDockerPluginFunctionalTest

---
+++ src/test/kotlin/dev/monosoul/jooq/functional/RunContainerWithCommandJooqDockerPluginFunctionalTest.kt
@@ -0,0 +1,55 @@
+package dev.monosoul.jooq.functional
+
+import org.gradle.testkit.runner.TaskOutcome.SUCCESS
+import org.junit.jupiter.api.Test
+import strikt.api.expect
+import strikt.assertions.isEqualTo
+import strikt.java.exists
+
+class RunContainerWithCommandJooqDockerPluginFunctionalTest : JooqDockerPluginFunctionalTestBase() {
+
+    @Test
+    fun `should support running DB containers with command`() {
+        // given
+        prepareBuildGradleFile {
+            """
+                plugins {
+                    id("dev.monosoul.jooq-docker")
+                }
+
+                repositories {
+                    mavenCentral()
+                }
+                
+                jooq {
+                    image {
+                        command = "postgres -p 6666"
+                    }
+
+                    db {
+                        port = 6666
+                    }
+                }
+
+                dependencies {
+                    jdbc("org.postgresql:postgresql:42.3.6")
+                }
+            """.trimIndent()
+        }
+        copyResource(from = "/V01__init.sql", to = "src/main/resources/db/migration/V01__init.sql")
+
+        // when
+        val result = runGradleWithArguments("generateJooqClasses")
+
+        // then
+        expect {
+            that(result).generateJooqClassesTask.outcome isEqualTo SUCCESS
+            that(
+                projectFile("build/generated-jooq/org/jooq/generated/tables/Foo.java")
+            ).exists()
+            that(
+                projectFile("build/generated-jooq/org/jooq/generated/tables/FlywaySchemaHistory.java")
+            ).exists()
+        }
+    }
+}

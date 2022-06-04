18eccf7a16fb7bde74b7156dfd2ccead5ac4e14d
monosoul <Kloz.Klaud@gmail.com>
2022-06-04 15:33:06 +0200

    Add unused suppression

--- src/main/kotlin/dev/monosoul/jooq/JooqExtension.kt
+++ src/main/kotlin/dev/monosoul/jooq/JooqExtension.kt
@@ -10,16 +10,20 @@ import java.io.Serializable
 open class JooqExtension : Serializable {
     internal var pluginSettings: JooqDockerPluginSettings = WithContainer.new()
 
+    @Suppress("unused")
     fun withContainer(configure: Action<WithContainer>) {
         pluginSettings = WithContainer.new(configure)
     }
 
+    @Suppress("unused")
     fun withContainer(closure: Closure<WithContainer>) = withContainer(closure::callWith)
 
+    @Suppress("unused")
     fun withoutContainer(configure: Action<WithoutContainer>) {
         pluginSettings = WithoutContainer.new(configure)
     }
 
+    @Suppress("unused")
     fun withoutContainer(closure: Closure<WithoutContainer>) = withoutContainer(closure::callWith)
 }
 

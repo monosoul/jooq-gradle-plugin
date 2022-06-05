3ddd127686bf768bac187927df8a85420efb0d0e
monosoul <Kloz.Klaud@gmail.com>
2022-06-05 13:49:13 +0200

    Add MultipleDatabasesJooqDockerPluginFunctionalTest

---
+++ src/test/kotlin/dev/monosoul/jooq/functional/MultipleDatabasesJooqDockerPluginFunctionalTest.kt
@@ -0,0 +1,94 @@
+package dev.monosoul.jooq.functional
+
+import org.gradle.testkit.runner.TaskOutcome.SUCCESS
+import org.junit.jupiter.api.Test
+import strikt.api.expect
+import strikt.assertions.isEqualTo
+import strikt.java.exists
+
+class MultipleDatabasesJooqDockerPluginFunctionalTest : JooqDockerPluginFunctionalTestBase() {
+
+    @Test
+    fun `should generate jooq classes for PostgreSQL and MySQL`() {
+        // given
+        prepareBuildGradleFile {
+            """
+                import dev.monosoul.jooq.GenerateJooqClassesTask
+                
+                plugins {
+                    kotlin("jvm") version "1.6.21"
+                    id("dev.monosoul.jooq-docker")
+                }
+
+                repositories {
+                    mavenCentral()
+                }
+                
+                tasks {
+                    generateJooqClasses {
+                        basePackageName = "org.jooq.generated.postgres"
+                        inputDirectory.setFrom("src/main/resources/postgres/migration")
+                        outputDirectory.set(project.layout.buildDirectory.dir("postgres"))
+                    }
+                    
+                    create<GenerateJooqClassesTask>("generateJooqClassesForMySql") {
+                        basePackageName = "org.jooq.generated.mysql"
+                        inputDirectory.setFrom("src/main/resources/mysql/migration")
+                        outputDirectory.set(project.layout.buildDirectory.dir("mysql"))
+                    
+                        withContainer {
+                            image {
+                                name = "mysql:8.0.29"
+                                envVars = mapOf(
+                                    "MYSQL_ROOT_PASSWORD" to "mysql",
+                                    "MYSQL_DATABASE" to "mysql"
+                                )
+                            }
+                            db {
+                                username = "root"
+                                password = "mysql"
+                                name = "mysql"
+                                port = 3306
+                            
+                                jdbc {
+                                    schema = "jdbc:mysql"
+                                    driverClassName = "com.mysql.cj.jdbc.Driver"
+                                }
+                            }
+                        }
+                    }
+                }
+
+                dependencies {
+                    implementation(kotlin("stdlib"))
+                    jdbc("org.postgresql:postgresql:42.3.6")
+                    jdbc("mysql:mysql-connector-java:8.0.29")
+                    implementation("org.jooq:jooq:3.16.6")
+                    implementation("javax.annotation:javax.annotation-api:1.3.2")
+                }
+            """.trimIndent()
+        }
+        copyResource(from = "/V01__init.sql", to = "src/main/resources/postgres/migration/V01__init.sql")
+        copyResource(from = "/V01__init_mysql.sql", to = "src/main/resources/mysql/migration/V01__init.sql")
+
+        // when
+        val result = runGradleWithArguments("classes")
+
+        // then
+        expect {
+            that(result).generateJooqClassesTask.outcome isEqualTo SUCCESS
+            that(
+                projectFile("build/postgres/org/jooq/generated/postgres/tables/Foo.java")
+            ).exists()
+            that(
+                projectFile("build/postgres/org/jooq/generated/postgres/tables/FlywaySchemaHistory.java")
+            ).exists()
+            that(
+                projectFile("build/mysql/org/jooq/generated/mysql/tables/Foo.java")
+            ).exists()
+            that(
+                projectFile("build/mysql/org/jooq/generated/mysql/tables/FlywaySchemaHistory.java")
+            ).exists()
+        }
+    }
+}

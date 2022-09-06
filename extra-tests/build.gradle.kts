15521f1ddfdbb69fc56f8ab1eb9a84b2d008a92a
monosoul <Kloz.Klaud@gmail.com>
2022-09-06 13:59:05 +0200

    Add a link to Gradle's doc

--- extra-tests/build.gradle.kts
+++ extra-tests/build.gradle.kts
@@ -7,6 +7,8 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 /**
  * This module is required because Gradle doesn't support Java agents
  * when using TestKit with configuration cache enabled
+ *
+ * https://docs.gradle.org/7.5.1/userguide/configuration_cache.html#config_cache:not_yet_implemented:testkit_build_with_java_agent
  */
 
 plugins {

df5a410764136c642e5fce0b23b757cf01ac0bea
Andrei Nevedomskii <kloz.klaud@gmail.com>
2022-09-06 15:58:19 +0200

    fix: disable test fixtures publishing (#48)

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,14 @@ plugins {
     `java-test-fixtures`
 }
 
+/**
+ * disable test fixtures publishing
+ * https://docs.gradle.org/current/userguide/java_testing.html#publishing_test_fixtures
+ */
+val javaComponent = components["java"] as AdhocComponentWithVariants
+javaComponent.withVariantsFromConfiguration(configurations["testFixturesApiElements"]) { skip() }
+javaComponent.withVariantsFromConfiguration(configurations["testFixturesRuntimeElements"]) { skip() }
+
 repositories {
     mavenCentral()
 }

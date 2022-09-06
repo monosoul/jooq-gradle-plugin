613c8ea4293c5b425333afc1081538f00bbc5b73
monosoul <Kloz.Klaud@gmail.com>
2022-09-06 13:54:47 +0200

    Better cover configuration cache

--- extra-tests/src/test/kotlin/dev/monosoul/jooq/functional/ConfigurationCacheJooqDockerPluginFunctionalTest.kt
+++ extra-tests/src/test/kotlin/dev/monosoul/jooq/functional/ConfigurationCacheJooqDockerPluginFunctionalTest.kt
@@ -2,15 +2,22 @@ package dev.monosoul.jooq.functional
 
 import org.gradle.testkit.runner.TaskOutcome.SUCCESS
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.io.TempDir
 import strikt.api.expect
+import strikt.assertions.contains
 import strikt.assertions.isEqualTo
 import strikt.java.exists
+import java.io.File
 
 class ConfigurationCacheJooqDockerPluginFunctionalTest : FunctionalTestBase() {
+
+    @TempDir
+    private lateinit var localBuildCacheDirectory: File
 
     @Test
     fun `should work with configuration cache enabled`() {
         // given
+        configureLocalGradleCache()
         prepareBuildGradleFile {
             """
                 plugins {
@@ -29,14 +36,37 @@ class ConfigurationCacheJooqDockerPluginFunctionalTest : FunctionalTestBase() {
         copyResource(from = "/V01__init.sql", to = "src/main/resources/db/migration/V01__init.sql")
 
         // when
+        // first run saves configuration cache
         val result = runGradleWithArguments("generateJooqClasses", "--configuration-cache")
+
+        // second run uses configuration cache
+        val resultFromCache = runGradleWithArguments("generateJooqClasses", "--configuration-cache")
 
         // then
         expect {
-            that(result).generateJooqClassesTask.outcome isEqualTo SUCCESS
+            that(result).apply {
+                generateJooqClassesTask.outcome isEqualTo SUCCESS
+                get { output }.contains("Configuration cache entry stored")
+            }
+            that(resultFromCache).apply {
+                generateJooqClassesTask.outcome isEqualTo SUCCESS
+                get { output }.contains("Configuration cache entry reused")
+            }
             that(
                 projectFile("build/generated-jooq/org/jooq/generated/tables/Foo.java")
             ).exists()
+        }
+    }
+
+    private fun configureLocalGradleCache() {
+        writeProjectFile("settings.gradle.kts") {
+            """
+                buildCache {
+                    local {
+                        directory = "${localBuildCacheDirectory.path}"
+                    }
+                }
+            """.trimIndent()
         }
     }
 }

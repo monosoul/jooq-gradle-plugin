46d4f8825691d8291c19aba66d871cc1c883216b
monosoul <Kloz.Klaud@gmail.com>
2022-12-04 02:44:30 +0100

    Add tasks configuration

--- smoke-tests/build.gradle.kts
+++ smoke-tests/build.gradle.kts
@@ -1,6 +1,9 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
 plugins {
     kotlin("jvm")
     `kotlin-convention`
+    alias(libs.plugins.shadow) apply false
 }
 
 dependencies {
@@ -10,4 +13,22 @@ dependencies {
     testImplementation(enforcedPlatform(libs.junit.bom))
     testImplementation(libs.junit.jupiter)
     testImplementation(libs.strikt)
+}
+
+tasks {
+    val shadowJar: ShadowJar by rootProject.tasks
+
+    val copyJar by registering(Copy::class) {
+        inputs.files(shadowJar)
+        from(shadowJar)
+        into("$buildDir/libs")
+
+        val newJarName = "plugin.jar"
+        rename("${rootProject.name}(\\.jar|\\-\\d+\\.\\d+\\.\\d+\\.jar)", newJarName)
+        outputs.files("$buildDir/libs/$newJarName")
+    }
+
+    test {
+        inputs.files(copyJar)
+    }
 }

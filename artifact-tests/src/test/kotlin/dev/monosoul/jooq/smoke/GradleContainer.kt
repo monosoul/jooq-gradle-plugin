8db93d947e1e8a1086f4a303528f38d920d78884
monosoul <Kloz.Klaud@gmail.com>
2022-12-04 05:46:45 +0100

    GradleContainer: make output reading thread-safe

--- artifact-tests/src/test/kotlin/dev/monosoul/jooq/smoke/GradleContainer.kt
+++ artifact-tests/src/test/kotlin/dev/monosoul/jooq/smoke/GradleContainer.kt
@@ -2,7 +2,7 @@ package dev.monosoul.jooq.smoke
 
 import ch.qos.logback.classic.Logger
 import ch.qos.logback.classic.spi.ILoggingEvent
-import ch.qos.logback.core.AppenderBase
+import ch.qos.logback.core.read.ListAppender
 import org.slf4j.LoggerFactory
 import org.testcontainers.containers.BindMode.READ_ONLY
 import org.testcontainers.containers.GenericContainer
@@ -10,20 +10,14 @@ import org.testcontainers.containers.SelinuxContext.SHARED
 import org.testcontainers.containers.output.Slf4jLogConsumer
 import org.testcontainers.containers.wait.strategy.LogMessageWaitStrategy
 import org.testcontainers.utility.MountableFile
-import java.util.Collections.synchronizedList
 
 class GradleContainer : GenericContainer<GradleContainer>("gradle:7.6.0-jdk17-alpine") {
 
-    private val rawOutput = synchronizedList(mutableListOf<ILoggingEvent>())
-    private val listAppender = object : AppenderBase<ILoggingEvent>() {
-        override fun append(eventObject: ILoggingEvent) {
-            rawOutput.add(eventObject)
-        }
-    }.also { it.start() }
+    private val listAppender = ListAppender<ILoggingEvent>().also { it.start() }
     private val logger = (LoggerFactory.getLogger("GradleContainer[$dockerImageName]") as Logger).also {
         it.addAppender(listAppender)
     }
-    val output: List<String> get() = rawOutput.map { it.formattedMessage }
+    val output: List<String> get() = ArrayList(listAppender.list).map { it.formattedMessage }
 
     init {
         withLogConsumer(

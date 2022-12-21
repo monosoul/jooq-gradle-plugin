ee68b7fa76eda7946197122ae83019ed7602730a
monosoul <Kloz.Klaud@gmail.com>
2022-12-21 15:46:30 +0100

    chore: populate pom.xml with extra info

--- buildSrc/src/main/kotlin/publishing-convention.gradle.kts
+++ buildSrc/src/main/kotlin/publishing-convention.gradle.kts
@@ -2,27 +2,59 @@ plugins {
     id("com.gradle.plugin-publish")
 }
 
+val siteUrl = "https://github.com/monosoul/jooq-gradle-plugin"
+val githubUrl = "https://github.com/monosoul/jooq-gradle-plugin"
+
+val pluginName = "jOOQ Docker Plugin"
+val pluginDescription = "Generates jOOQ classes using dockerized database"
+
 gradlePlugin {
+    website.set(siteUrl)
+    vcsUrl.set(githubUrl)
+
     plugins.create("jooqDockerPlugin") {
         id = "dev.monosoul.jooq-docker"
         implementationClass = "dev.monosoul.jooq.JooqDockerPlugin"
         version = project.version
 
-        displayName = "jOOQ Docker Plugin"
-        description = "Generates jOOQ classes using dockerized database"
+        displayName = pluginName
+        description = pluginDescription
+
+        tags.set(listOf("jooq", "docker", "db"))
     }
 }
 
-pluginBundle {
-    website = "https://github.com/monosoul/jooq-gradle-plugin"
-    vcsUrl = "https://github.com/monosoul/jooq-gradle-plugin"
-
-    pluginTags = mapOf(
-        "jooqDockerPlugin" to listOf("jooq", "docker", "db"),
-    )
-}
-
 publishing {
+    publications {
+        withType<MavenPublication>() {
+            pom {
+                name.set(pluginName)
+                description.set(pluginDescription)
+                url.set(siteUrl)
+                scm {
+                    url.set(githubUrl)
+                    connection.set("scm:git:$githubUrl.git")
+                    developerConnection.set("scm:git:$githubUrl.git")
+                }
+                developers {
+                    developer {
+                        id.set("monosoul")
+                        name.set("Andrei Nevedomskii")
+                    }
+                }
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                issueManagement {
+                    url.set("$githubUrl/issues")
+                }
+            }
+        }
+    }
+
     repositories {
         maven {
             name = "Snapshot"

0ec9b4c5b97af4d138c35d75c5efa7dbbd99679e
Andrei Nevedomskii <kloz.klaud@gmail.com>
2023-05-02 13:29:27 +0200

    chore: update Gradle to 8.1.1 along with plugins (#116)

--- buildSrc/src/main/kotlin/shadow-convention.gradle.kts
+++ buildSrc/src/main/kotlin/shadow-convention.gradle.kts
@@ -1,18 +1,14 @@
-import com.github.jengelman.gradle.plugins.shadow.tasks.ConfigureShadowRelocation
-import org.gradle.kotlin.dsl.kotlin
-
 plugins {
     kotlin("jvm")
     id("com.github.johnrengelman.shadow")
 }
 
 tasks {
-    val relocateShadowJar by registering(ConfigureShadowRelocation::class) {
-        target = shadowJar.get()
-        prefix = "${project.group}.shadow"
-    }
 
     shadowJar {
+        isEnableRelocation = true
+        relocationPrefix = "${project.group}.shadow"
+
         mergeServiceFiles()
 
         fun inMetaInf(vararg patterns: String) = patterns.map { "META-INF/$it" }.toTypedArray()
@@ -21,7 +17,5 @@ tasks {
             *inMetaInf("maven/**", "NOTICE*", "README*", "CHANGELOG*", "DEPENDENCIES*", "LICENSE*", "ABOUT*"),
             "LICENSE*",
         )
-
-        dependsOn(relocateShadowJar)
     }
 }

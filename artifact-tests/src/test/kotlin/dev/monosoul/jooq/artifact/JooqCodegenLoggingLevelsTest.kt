386d901c0494afaf64c6081264ab86623e67ef30
monosoul <Kloz.Klaud@gmail.com>
2023-07-16 18:12:40 +0200

    fix: prevent Gradle from trying to download dependencies from the plugin portal

--- artifact-tests/src/test/kotlin/dev/monosoul/jooq/artifact/JooqCodegenLoggingLevelsTest.kt
+++ artifact-tests/src/test/kotlin/dev/monosoul/jooq/artifact/JooqCodegenLoggingLevelsTest.kt
@@ -96,16 +96,24 @@ class JooqCodegenLoggingLevelsTest {
         withCopyToContainer(
             Transferable.of(
                 """
-                    rootProject.name = "testproject"
-
                     pluginManagement {
                         repositories {
                             maven {
                                 name = "localBuild"
                                 url = uri("./local-repository")
                             }
-                            gradlePluginPortal()
                             mavenCentral()
+                            gradlePluginPortal {
+                                content {
+                                    excludeGroup("org.jooq")
+                                    excludeGroup("org.flywaydb")
+                                    excludeGroupByRegex("com\\.fasterxml.*")
+                                    excludeGroupByRegex("com\\.google.*")
+                                    excludeGroupByRegex("org\\.junit.*")
+                                    excludeGroupByRegex("net\\.java.*")
+                                    excludeGroupByRegex("jakarta.*")
+                                }
+                            }
                         }
                     }
                 """.trimIndent()
--- artifact-tests/src/test/resources/testproject/settings.gradle.kts
+++ artifact-tests/src/test/resources/testproject/settings.gradle.kts
@@ -6,7 +6,17 @@ pluginManagement {
             name = "localBuild"
             url = uri("./local-repository")
         }
-        gradlePluginPortal()
         mavenCentral()
+        gradlePluginPortal {
+            content {
+                excludeGroup("org.jooq")
+                excludeGroup("org.flywaydb")
+                excludeGroupByRegex("com\\.fasterxml.*")
+                excludeGroupByRegex("com\\.google.*")
+                excludeGroupByRegex("org\\.junit.*")
+                excludeGroupByRegex("net\\.java.*")
+                excludeGroupByRegex("jakarta.*")
+            }
+        }
     }
 }

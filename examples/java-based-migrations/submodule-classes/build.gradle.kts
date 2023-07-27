960e91ef4840ed2d1e33273e03bc8a34fa814a37
monosoul <Kloz.Klaud@gmail.com>
2023-07-27 16:51:27 +0200

    chore: typo fix

--- examples/java-based-migrations/submodule-classes/build.gradle.kts
+++ examples/java-based-migrations/submodule-classes/build.gradle.kts
@@ -1,7 +1,7 @@
 /**
  * This is how you can configure the plugin to use Java-based migrations from a submodule.
  *
- * This is a preferred method of setting it up when your migrations are located in teh same project,
+ * This is a preferred method of setting it up when your migrations are located in the same project,
  * because this way Gradle cache will work properly for generateJooqClasses task.
  */
 

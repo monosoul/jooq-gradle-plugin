302ebbb1739e12790b6ee723c48ef756fb6f4414
monosoul <Kloz.Klaud@gmail.com>
2023-11-04 18:55:46 +0100

    add coverage caching issue workaround

--- buildSrc/src/main/kotlin/coverage-convention.gradle.kts
+++ buildSrc/src/main/kotlin/coverage-convention.gradle.kts
@@ -1,3 +1,6 @@
+import java.lang.Thread.sleep
+import java.time.Duration
+
 plugins {
     kotlin("jvm")
     jacoco
@@ -11,5 +14,14 @@ tasks {
             html.required.set(false)
         }
         dependsOn(withType<Test>())
+    }
+
+    withType<Test> {
+        // workaround for https://github.com/gradle/gradle/issues/16603
+        doLast {
+            sleep(
+                Duration.ofSeconds(2).toMillis()
+            )
+        }
     }
 }

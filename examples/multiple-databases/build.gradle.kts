f8aff1aefb293ec3c0ad606f99634d5fb5c214dc
monosoul <kloz.klaud@gmail.com>
2024-12-21 12:23:43 +0100

    chore: bump dependency versions

--- examples/multiple-databases/build.gradle.kts
+++ examples/multiple-databases/build.gradle.kts
@@ -6,8 +6,8 @@ import dev.monosoul.jooq.GenerateJooqClassesTask
 import dev.monosoul.jooq.RecommendedVersions
 
 plugins {
-    kotlin("jvm") version "1.9.20"
-    id("dev.monosoul.jooq-docker") version "6.0.0"
+    kotlin("jvm") version "2.1.0"
+    id("dev.monosoul.jooq-docker") version "6.1.14"
 }
 
 repositories {
--- examples/spring-boot/recommended-jooq-version/build.gradle.kts
+++ examples/spring-boot/recommended-jooq-version/build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
     val kotlinVersion = "1.7.22"
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.spring") version kotlinVersion
-    id("dev.monosoul.jooq-docker") version "6.0.0"
+    id("dev.monosoul.jooq-docker") version "6.1.14"
 }
 
 repositories {
